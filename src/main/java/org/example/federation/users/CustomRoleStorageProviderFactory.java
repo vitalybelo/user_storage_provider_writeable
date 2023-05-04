6351c209719dde05fdef1699ade1ffe0ae8bc076
vital <vitaly.belo@gmail.com>
2023-05-04 18:10:43 +0300

    CHECKPOINT #6: добавлен провайдер ролей

--- src/main/java/org/example/federation/users/CustomRoleStorageProviderFactory.java
+++ src/main/java/org/example/federation/users/CustomRoleStorageProviderFactory.java
@@ -1,12 +1,15 @@
 package org.example.federation.users;
 
+import org.keycloak.Config;
 import org.keycloak.component.ComponentModel;
 import org.keycloak.models.KeycloakSession;
+import org.keycloak.models.KeycloakSessionFactory;
 import org.keycloak.storage.role.RoleStorageProviderFactory;
 
 public class CustomRoleStorageProviderFactory implements RoleStorageProviderFactory<CustomRoleStorageProvider> {
 
     public static final String PROVIDER_ID = "JDBC ROLES";
+
 
     @Override
     public CustomRoleStorageProvider create(KeycloakSession session, ComponentModel model) {
@@ -14,7 +17,23 @@ public class CustomRoleStorageProviderFactory implements RoleStorageProviderFact
     }
 
     @Override
+    public void init(Config.Scope config) {
+
+    }
+
+    @Override
+    public void postInit(KeycloakSessionFactory factory) {
+
+    }
+
+    @Override
+    public void close() {
+
+    }
+
+    @Override
     public String getId() {
         return PROVIDER_ID;
     }
+
 }
