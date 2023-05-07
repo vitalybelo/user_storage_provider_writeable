1fd01076ecb22e65bcb7e7a32f2e1aa37c52b24d
vital <vitaly.belo@gmail.com>
2023-05-07 16:16:29 +0300

    CHECKPOINT #6: добавлен RoleStorageProvider, RoleProvider - перехвата метода addRealmRole НЕТ !!!

--- src/main/java/org/example/federation/users/CustomRoleStorage.java
+++ src/main/java/org/example/federation/users/CustomRoleStorage.java
@@ -15,7 +15,6 @@ import java.util.HashSet;
 import java.util.List;
 import java.util.stream.Collectors;
 import java.util.Set;
-import java.util.stream.Stream;
 
 @Slf4j
 public class CustomRoleStorage {
